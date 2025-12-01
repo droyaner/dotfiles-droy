35f183048cb3b69de112fa41ea2c1d94452ad984
Marcus Treu <marcus.treu@mail.de>
2025-12-01 23:24:54 +0100

    add nvim correct

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- fzf lua
+vim.keymap.set("n", "<leader>fs", ":FzfLua files<cr>")
+vim.keymap.set("n", "<leader>fz", ":FzfLua live_grep<cr>")
+vim.keymap.set("n", "<leader>fo", ":FzfLua oldfiles<cr>")
+
+-- tree
+vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<cr>")
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,4 @@
+vim.cmd("colorscheme tokyonight-moon") -- set color theme
+
+-- vim.opt.termguicolors = true --bufferline
+require("bufferline").setup{} --bufferline

5661e6e870455568d41083e7efd9636f5702ae15
Marcus Treu <marcus.treu@mail.de>
2026-05-25 23:23:58 +0200

    undo wezterm changes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -69,13 +69,13 @@ config.window_decorations = "TITLE | RESIZE"
 config.window_background_opacity = 0.99
 
 -- Always start windows maximized
-wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
-end)
-
-wezterm.on("window-config-reloaded", function(window, pane)
-	window:maximize()
+wezterm.on("gui-attached", function(domain)
+	local workspace = wezterm.mux.get_active_workspace()
+	for _, window in ipairs(wezterm.mux.all_windows()) do
+		if window:gui_window() then
+			window:gui_window():maximize()
+		end
+	end
 end)
 
 return config

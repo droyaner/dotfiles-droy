b4a96b3e9c3573af413eb04dd5978e241d1be80b
Marcus Treu <marcus.treu@mail.de>
2026-07-13 19:37:40 +0200

    nvim: add git to which key

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"folke/which-key.nvim",
 		version = "*",
-		event = "VeryLazy",
+		lazy = false,
 		config = function()
 			local wk = require("which-key")
 
@@ -31,6 +31,17 @@ return {
 				{ "<leader>b", group = "Buffers" },
 				{ "<leader>g", group = "Git" },
 				{ "<leader>gt", group = "Git Toggle" },
+				{ "<leader>gs", desc = "Stage hunk", mode = { "n", "v" } },
+				{ "<leader>gr", desc = "Reset hunk", mode = { "n", "v" } },
+				{ "<leader>gS", desc = "Stage buffer" },
+				{ "<leader>gu", desc = "Undo stage hunk" },
+				{ "<leader>gR", desc = "Reset buffer" },
+				{ "<leader>gp", desc = "Preview hunk" },
+				{ "<leader>gb", desc = "Blame line" },
+				{ "<leader>gd", desc = "Diff this" },
+				{ "<leader>gD", desc = "Diff this ~" },
+				{ "<leader>gtd", desc = "Toggle deleted" },
+				{ "<leader>gtb", desc = "Toggle line blame" },
 				{ "<leader>e", desc = "File Explorer" },
 				{ "g", group = "Go to (LSP)" },
 				{ "[", group = "Previous" },

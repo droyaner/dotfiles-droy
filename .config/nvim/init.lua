501d84d93b1dea3474a72e144a6cb9261b4ec5e7
Marcus Treu <marcus.treu@mail.de>
2026-08-30 08:00:50 +0200

    add filetype plogin indent

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,14 @@
 -- Load config modules
+vim.cmd("filetype plugin indent on")
+vim.filetype.add({
+	extension = {
+		hpp = "cpp",
+	},
+	filename = {
+		[".zshrc"] = "zsh",
+	},
+})
+
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
